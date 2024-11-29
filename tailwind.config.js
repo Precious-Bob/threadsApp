1c54532afa3fa6223844d46e2e34dcc10a311c77
Precious <preciousbobmanuel501@gmail.com>
2024-11-29 15:56:57 +0100

    fixing css...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,ts,scss}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
